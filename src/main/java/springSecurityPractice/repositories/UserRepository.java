bc2c3e30efcb186969af6c16dc85b24d215fade1
Ketan R <ketan_r598@outlook.com>
2024-03-17 19:07:50 +0530

    added findByEmail method to the user_repository

--- src/main/java/springSecurityPractice/repositories/UserRepository.java
+++ src/main/java/springSecurityPractice/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package springSecurityPractice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import springSecurityPractice.models.User;
 
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
 }
