bfdd29c0fab0cc60e1e0ce691de4a305a018d23a
Ketan R <ketan_r598@outlook.com>
2024-03-20 15:00:11 +0530

    added enable field to the user model

--- src/main/java/springSecurityPractice/models/User.java
+++ src/main/java/springSecurityPractice/models/User.java
@@ -7,11 +7,9 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import springSecurityPractice.customValidators.ValidEmail;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "users")
 public class User extends BaseModel{
@@ -19,6 +17,7 @@ public class User extends BaseModel{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long user_id;
+	
 	
 	@NotBlank
 	private String firstName;
@@ -29,6 +28,13 @@ public class User extends BaseModel{
 	@NotBlank
 	private String password;
 	
+	private boolean enabled;
+	
 	@ValidEmail
 	private String email;
+	
+	public User() {
+		super();
+		this.enabled = false;
+	}
 }
