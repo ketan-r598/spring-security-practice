c6e9bd96cffb014a2e648f4a7688c8de00f4464b
Ketan R <ketan_r598@outlook.com>
2024-03-21 00:39:21 +0530

    added isValid attribute to the verificationToken model

--- src/main/java/springSecurityPractice/models/VerificationToken.java
+++ src/main/java/springSecurityPractice/models/VerificationToken.java
@@ -13,10 +13,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "tokens")
 public class VerificationToken extends BaseModel{
@@ -35,7 +33,8 @@ public class VerificationToken extends BaseModel{
 	
 	private Date expiryDate;
 	
-	@SuppressWarnings("unused")
+	private boolean isValid;
+	
 	private Date calculateExpiryDate(int expiryTimeInMinutes) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(new Timestamp(calendar.getTime().getTime()));
@@ -45,5 +44,9 @@ public class VerificationToken extends BaseModel{
 	
 	public void setExpiryDate() {
 		this.expiryDate = calculateExpiryDate(EXPIRATION);
+	}
+	
+	public VerificationToken() {
+		this.isValid = true;
 	}
 }
