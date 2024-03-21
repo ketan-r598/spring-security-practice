7df8c785b78ab0347d5ab552a09058380f332acc
Ketan R <ketan_r598@outlook.com>
2024-03-21 18:33:28 +0530

    added password reset token model

---
+++ src/main/java/springSecurityPractice/models/PasswordResetToken.java
@@ -0,0 +1,53 @@
+package springSecurityPractice.models;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class PasswordResetToken extends BaseModel {
+	
+	private static final int EXPIRATION = 60 * 24;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long passwordResetTokenId;
+	
+	private String token;
+	
+	@OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	private Date expiryDate;
+	
+	private boolean isValid;
+	
+	private Date calculateExpiryDate(int expiryTimeInMinutes) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Timestamp(calendar.getTime().getTime()));
+		calendar.add(Calendar.MINUTE, expiryTimeInMinutes);
+		return new Date(calendar.getTime().getTime());
+	}
+	
+	public void setExpiryDate() {
+		this.expiryDate = calculateExpiryDate(EXPIRATION);
+	}
+	
+	public PasswordResetToken() {
+		this.isValid = true;
+	}
+	
+}
