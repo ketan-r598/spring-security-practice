d70b30d850f48ef456c5a2348e3ccda8a83903d6
Ketan R <ketan_r598@outlook.com>
2024-03-21 18:40:47 +0530

    added methods to IUSerService interface

--- src/main/java/springSecurityPractice/services/IUserService.java
+++ src/main/java/springSecurityPractice/services/IUserService.java
@@ -6,6 +6,7 @@ import springSecurityPractice.dtos.UserRequestDTO;
 import springSecurityPractice.dtos.UserResponseDTO;
 import springSecurityPractice.exceptions.UserAlreadyExistException;
 import springSecurityPractice.exceptions.UserNotFoundException;
+import springSecurityPractice.models.PasswordResetToken;
 import springSecurityPractice.models.User;
 import springSecurityPractice.models.VerificationToken;
 
@@ -15,7 +16,11 @@ public interface IUserService {
 	public Optional<UserResponseDTO> getUser(String email) throws UserNotFoundException;
 	public User getUserUser(String email);
 	public void saveRegisteredUser(User user);
+	
 	public void createVerificationToken(User user, String token);
 	public VerificationToken getVerificationToken(String verificationToken);
+	
+	public void createPasswordResetToken(User user, String token);
+	public PasswordResetToken getPasswordResetToken(String passwordResetToken);
 	
 }
